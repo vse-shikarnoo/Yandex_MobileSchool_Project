9363b2218e6fce3e2757f7fed1f68d0cc9fd037f
Vladimir <vova_klushin@mail.ru>
2025-06-11 17:59:17 +0300

    make topbar state

--- app/src/main/java/yandex/school/project/ui/screens/MainScreen.kt
+++ app/src/main/java/yandex/school/project/ui/screens/MainScreen.kt
@@ -1,9 +1,5 @@
 package yandex.school.project.ui.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
@@ -16,54 +12,76 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.navigation.compose.rememberNavController
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import yandex.school.project.R
 import yandex.school.project.ui.navigation.BottomBar
 import yandex.school.project.ui.navigation.BottomNavigation
+
+data class TopBarState(
+    val navigationIcon: ImageVector?,
+    val navigationIconAction: () -> Unit = {},
+    val title: String,
+    val actionIcon: ImageVector?,
+    val actionIconAction: () -> Unit = {},
+)
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     val bottomNavController = rememberNavController()
 
-    val currentTitle = rememberSaveable() { mutableStateOf("") }
+    val currentTopBarState = remember() {
+        mutableStateOf(
+            TopBarState(
+                navigationIcon = null,
+                title = "",
+                actionIcon = null,
+            )
+        )
+    }
 
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
                 navigationIcon = {
-                    IconButton(
-                        onClick = {}
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Close,
-                            null
-                        )
+                    if (currentTopBarState.value.navigationIcon != null) {
+                        IconButton(
+                            onClick = currentTopBarState.value.navigationIconAction
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Close,
+                                null
+                            )
+                        }
                     }
                 },
                 title = {
-                    Text(text = currentTitle.value,
-                        style = MaterialTheme.typography.titleLarge)
+                    Text(
+                        text = currentTopBarState.value.title,
+                        style = MaterialTheme.typography.titleLarge
+                    )
                 },
                 actions = {
-                    IconButton(
-                        onClick = {}
-                    ) {
-                        Icon(
-                            imageVector = ImageVector.vectorResource(R.drawable.ic_history),
-                            null
-                        )
+                    if (currentTopBarState.value.actionIcon != null) {
+                        IconButton(
+                            onClick = currentTopBarState.value.actionIconAction
+                        ) {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(R.drawable.ic_history),
+                                null
+                            )
+                        }
                     }
                 },
                 colors = TopAppBarDefaults.topAppBarColors(
@@ -88,7 +106,7 @@ fun MainScreen() {
         Box(modifier = Modifier.padding(innerPadding)) {
             BottomNavigation(
                 navController = bottomNavController,
-                onTitleChange = { currentTitle.value = it }
+                onTitleChange = { currentTopBarState.value = it }
             )
         }
     }
