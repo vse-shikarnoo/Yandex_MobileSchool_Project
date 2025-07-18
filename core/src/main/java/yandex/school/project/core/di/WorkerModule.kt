f80624e9f0eebbea76ba0cba42346191de6555ee
Klushin Valdimir <vova_klushin@mail.ru>
2025-07-18 22:07:15 +0300

    fix

--- core/src/main/java/yandex/school/project/core/di/WorkerModule.kt
+++ core/src/main/java/yandex/school/project/core/di/WorkerModule.kt
@@ -2,6 +2,7 @@ package yandex.school.project.core.di
 
 import dagger.Module
 import dagger.Provides
+import yandex.school.project.core.data.repository.AccountRepositoryImpl
 import yandex.school.project.core.data.repository.TransactionRepositoryImpl
 import yandex.school.project.core.work.SyncTransactionsWorker
 import javax.inject.Provider
@@ -12,8 +13,9 @@ object WorkerModule {
     @Provides
     @Singleton
     fun provideSyncTransactionsWorkerFactory(
-        transactionRepositoryProvider: Provider<TransactionRepositoryImpl>
+        transactionRepositoryProvider: Provider<TransactionRepositoryImpl>,
+        accountRepositoryProvider: Provider<AccountRepositoryImpl>
     ): SyncTransactionsWorkerFactory {
-        return SyncTransactionsWorkerFactory(transactionRepositoryProvider)
+        return SyncTransactionsWorkerFactory(transactionRepositoryProvider, accountRepositoryProvider)
     }
 }
